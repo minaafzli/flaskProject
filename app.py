0ff2abf69accd757e6af7190ba84053cfdd4d360
minaafzli <mina.afzli81@gmail.com>
2024-01-04 03:33:58 +0330

    complete update and delet buttons

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class todo(db.Model):
+class Todo(db.Model):
     id =db.Column(db.Integer , primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
 @app.route('/')
 def index():
-    todo_list = todo.query.all()
+    todo_list = Todo.query.all()
     return render_template('base.html',todo_list = todo_list)
 
 @app.route('/about')
@@ -24,8 +24,25 @@ def about():
 def add():
     #adding new titles
     title = request.form.get("title")
-    new_todo = todo(title = "title" ,complete = False)
+    new_todo = Todo(title = title ,complete = False)
     db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+    
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    #update title
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    #delete titles
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
 
